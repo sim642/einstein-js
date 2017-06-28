9f1194d3040330906a6138d9e6a432b118cf9079
Simmo Saan <simmo.saan@gmail.com>
2017-06-28 10:31:54 +0300

    Target tablet screen additionally

--- src/index.tsx
+++ src/index.tsx
@@ -5,3 +5,6 @@ import {AppComponent} from "./web/AppComponent";
 let puzzle = Puzzle.generate();
 console.debug(puzzle);
 render(<AppComponent puzzle={puzzle}/>, document.body);
+
+/*var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+alert(width);*/
