a072badf271eb3e542eaf5e16a322dea9080d679
Simmo Saan <simmo.saan@gmail.com>
2017-06-29 15:35:19 +0300

    Split libraries to separate bundles, autogenerate index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 var path = require("path");
+var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: {
+        app: "./src/index.tsx"
+    },
+
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
     },
 
@@ -55,6 +60,24 @@ module.exports = {
     },
 
     plugins: [
-        new ExtractTextPlugin("styles.css")
+        new ExtractTextPlugin("styles.css"),
+
+        // https://webpack.js.org/guides/code-splitting-libraries/#manifest-file
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            minChunks: function (module) {
+                // this assumes your vendor imports exist in the node_modules directory
+                return module.context && module.context.indexOf("node_modules") !== -1;
+            }
+        }),
+        //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "manifest" //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+        }),
+
+        new HtmlWebpackPlugin({
+            title: "einstein-js",
+            template: "./src/index.ejs"
+        })
     ]
 };
