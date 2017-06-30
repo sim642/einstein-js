75f1185c9f35bc3b04a834138a41e74a841bc2c9
Simmo Saan <simmo.saan@gmail.com>
2017-06-30 15:24:28 +0300

    Initial build dist

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import {Component, h, render} from "preact";
-import {AppComponent} from "./web/AppComponent";
-
-render(<AppComponent/>, document.body);
-
-/*var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-alert(width);*/
