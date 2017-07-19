54caae89f403975fcb9b1eaf1c10c05d52b8da59
Simmo Saan <simmo.saan@gmail.com>
2017-07-19 15:42:32 +0300

    Parametrize SingleBoard#random() test

--- test/puzzle/board/SingleBoard.test.ts
+++ test/puzzle/board/SingleBoard.test.ts
@@ -6,32 +6,36 @@ import {SingleBoard} from "../../../src/puzzle/board/SingleBoard";
 
 describe("SingleBoard", function () {
     describe("#random()", function () {
-        const options: BoardOptions = {
-            rows: 6,
-            cols: 6
-        };
+        _.forEach([
+            {rows: 6, cols: 6},
+            {rows: 5, cols: 5},
+            {rows: 4, cols: 4},
+            {rows: 6, cols: 4}
+        ], (options: BoardOptions) => {
+            context(`with ${options.rows} rows, ${options.cols} cols`, function () {
+                it("should return correct size board", function () {
+                    let board = SingleBoard.random(options);
 
-        it("should return correct size board", function () {
-            let board = SingleBoard.random(options);
+                    expect(board.rows).to.equal(options.rows);
+                    expect(board.cols).to.equal(options.cols);
 
-            expect(board.rows).to.equal(options.rows);
-            expect(board.cols).to.equal(options.cols);
-
-            _.forEach(_.range(0, options.rows), row => {
-                _.forEach(_.range(0, options.cols), col => {
-                    expect(board.get(row, col)).to.be.a("number");
+                    _.forEach(_.range(0, options.rows), row => {
+                        _.forEach(_.range(0, options.cols), col => {
+                            expect(board.get(row, col)).to.be.a("number");
+                        });
+                    });
                 });
-            });
-        });
 
-        it("should return board with permutations as rows", function () {
-            let board = SingleBoard.random(options);
+                it("should return board with permutations as rows", function () {
+                    let board = SingleBoard.random(options);
 
-            expect(board.variants).to.equal(options.cols);
+                    expect(board.variants).to.equal(options.cols);
 
-            _.forEach(_.range(0, options.rows), row => {
-                let rowValues = _.map(_.range(0, options.cols), col => board.get(row, col));
-                expect(rowValues).to.have.members(_.range(0, options.cols));
+                    _.forEach(_.range(0, options.rows), row => {
+                        let rowValues = _.map(_.range(0, options.cols), col => board.get(row, col));
+                        expect(rowValues).to.have.members(_.range(0, options.cols));
+                    });
+                });
             });
         });
     });
