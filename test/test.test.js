9bd96c83fa2ecce2234146c74f14af2fb9d21af4
Simmo Saan <simmo.saan@gmail.com>
2017-07-19 13:02:07 +0300

    Add mocha-webpack for unit testing (#22)

---
+++ test/test.test.js
@@ -0,0 +1,11 @@
+var assert = require("assert");
+
+describe("Test", function () {
+   it("should succeed", function () {
+       assert(true);
+   });
+
+   it("should fail", function () {
+       assert(false);
+   });
+});
