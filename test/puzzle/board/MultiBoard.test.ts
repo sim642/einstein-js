bf0217b26058a57afb3be165c330501429b414ea
Simmo Saan <simmo.saan@gmail.com>
2017-07-19 15:55:54 +0300

    Test MultiBoard#full()

---
+++ test/puzzle/board/MultiBoard.test.ts
@@ -0,0 +1,44 @@
+import {expect} from "chai";
+import * as _ from "lodash";
+import "mocha";
+import {BoardOptions} from "../../../src/puzzle/board/Board";
+import {MultiBoard} from "../../../src/puzzle/board/MultiBoard";
+
+describe("MultiBoard", function () {
+    describe("#full()", function () {
+        _.forEach([
+            {rows: 6, cols: 6},
+            {rows: 5, cols: 5},
+            {rows: 4, cols: 4},
+            {rows: 6, cols: 4}
+        ], (options: BoardOptions) => {
+            context(`with ${options.rows} rows, ${options.cols} cols`, function () {
+                it("should return correct size board", function () {
+                    let board = MultiBoard.full(options);
+
+                    expect(board.rows).to.equal(options.rows);
+                    expect(board.cols).to.equal(options.cols);
+
+                    _.forEach(_.range(0, options.rows), row => {
+                        _.forEach(_.range(0, options.cols), col => {
+                            expect(board.get(row, col)).to.be.an("Array");
+                        });
+                    });
+                });
+
+                it("should return board with all variants", function () {
+                    let board = MultiBoard.full(options);
+
+                    expect(board.variants).to.equal(options.cols);
+
+                    const allVariants = _.times(options.cols, _.constant(true));
+                    _.forEach(_.range(0, options.rows), row => {
+                        _.forEach(_.range(0, options.cols), col => {
+                            expect(board.get(row, col)).to.deep.equal(allVariants);
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
