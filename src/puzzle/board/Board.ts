aff8b4c3e2e11894a1795b468e963098f68a620b
Simmo Saan <simmo.saan@gmail.com>
2017-07-21 13:55:12 +0300

    Simplify optional Board options using argument initializer

--- src/puzzle/board/Board.ts
+++ src/puzzle/board/Board.ts
@@ -4,24 +4,16 @@ export interface BoardOptions {
 }
 
 export abstract class Board<T> {
-    protected options: BoardOptions;
-    readonly rows: number;
-    readonly cols: number;
-    readonly variants: number;
+    constructor(private table: T[][], protected options: BoardOptions = {
+        rows: table.length,
+        cols: table[0].length
+    }) {
 
-    constructor(private table: T[][], options?: BoardOptions) {
-        if (!options) {
-            options = {
-                rows: table.length,
-                cols: table[0].length // assuming table is rectangular
-            };
-        }
-        this.options = options;
-
-        this.rows = options.rows;
-        this.cols = options.cols;
-        this.variants = options.cols;
     }
+
+    readonly rows: number = this.options.rows;
+    readonly cols: number = this.options.cols;
+    readonly variants: number = this.options.cols;
 
     get(row: number, col: number): T {
         return this.table[row][col];
