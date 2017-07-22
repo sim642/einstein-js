79755d85849828c345748ba68a38930185bb3eee
Simmo Saan <simmo.saan@gmail.com>
2017-07-22 08:23:51 +0300

    Test OpenHint

---
+++ test/puzzle/hints/OpenHint.test.ts
@@ -0,0 +1,39 @@
+import {expect} from "chai";
+import "mocha";
+import {MultiBoard} from "../../../src/puzzle/board/MultiBoard";
+import {OpenHint} from "../../../src/puzzle/hint/OpenHint";
+import {HintType} from "../../../src/puzzle/hint/Hint";
+
+describe("OpenHint", function () {
+    describe("#apply()", function () {
+        let board: MultiBoard;
+        beforeEach(function () {
+            board = MultiBoard.full({rows: 3, cols: 3});
+        });
+
+        it("should set cell variant", function () {
+            board.applyHint(new OpenHint(0, 1, 2));
+
+            expect(board.isSingle(0, 1)).to.be.true;
+            expect(board.getSingle(0, 1)).to.equal(2);
+        });
+
+        it("should return true if variant is set", function () {
+            let changed = board.applyHint(new OpenHint(0, 1, 2));
+
+            expect(changed).to.be.true;
+        });
+
+        it("should return false if variant already set", function () {
+            board.set(0, 1, 2);
+
+            let changed = board.applyHint(new OpenHint(0, 1, 2));
+
+            expect(changed).to.be.false;
+        });
+    });
+
+    it("should have Start type", function () {
+        expect(new OpenHint(0, 1, 2).getType()).to.equal(HintType.Start);
+    });
+});
