c4cf1a4343c0307ba3029a978c2999b53701358e
Simmo Saan <simmo.saan@gmail.com>
2017-08-26 13:54:03 +0300

    Extract random hint generation to RandomHintFactory

--- src/puzzle/Puzzle.ts
+++ src/puzzle/Puzzle.ts
@@ -2,12 +2,8 @@ import * as _ from "lodash";
 import {BoardOptions} from "./board/Board";
 import {MultiBoard} from "./board/MultiBoard";
 import {SingleBoard} from "./board/SingleBoard";
-import {AdjacentHintFactory} from "./hint/AdjacentHint";
-import {BetweenHintFactory} from "./hint/BetweenHint";
-import {DirectionHintFactory} from "./hint/DirectionHint";
 import {Hint, HintFactory, HintType} from "./hint/Hint";
-import {OpenHintFactory} from "./hint/OpenHint";
-import {SameColumnHintFactory} from "./hint/SameColumnHint";
+import {RandomHintFactory} from "./RandomHint";
 
 export interface PuzzleOptions extends BoardOptions {
 
@@ -38,53 +34,15 @@ export class Puzzle {
         return new Puzzle(board, Puzzle.pruneHints(board, Puzzle.generateHints(board)), options);
     }
 
+    private static hintFactory: HintFactory = new RandomHintFactory();
+
     private static generateHints(board: SingleBoard): Hint[] {
         let hints: Hint[] = [];
         while (!board.isSolvable(hints)) {
-            let hint = Puzzle.generateHint(board);
+            let hint = Puzzle.hintFactory.random(board);
             hints.push(hint);
         }
         return hints;
-    }
-
-    private static generateHint(board: SingleBoard): Hint {
-        let hintFactory: HintFactory;
-        // hint frequency distribution from original einstein 2.0
-        switch (_.random(0, 14 - 1)) {
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-                hintFactory = new AdjacentHintFactory();
-                break;
-
-            case 4:
-                hintFactory = new OpenHintFactory();
-                break;
-
-            case 5:
-            case 6:
-                hintFactory = new SameColumnHintFactory();
-                break;
-
-            case 7:
-            case 8:
-            case 9:
-            case 10:
-                hintFactory = new DirectionHintFactory();
-                break;
-
-            case 11:
-            case 12:
-            case 13:
-                hintFactory = new BetweenHintFactory();
-                break;
-
-            // istanbul ignore next: impossible case
-            default:
-                throw new Error("Unhandled random HintFactory value");
-        }
-        return hintFactory.random(board);
     }
 
     private static pruneHints(board: SingleBoard, hints: Hint[]): Hint[] {
