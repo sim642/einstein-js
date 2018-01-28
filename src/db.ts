564093d34cb2ca2e09c0b6e0302fc9ecd16ec478
Simmo Saan <simmo.saan@gmail.com>
2018-01-28 17:03:41 +0200

    Fix db creation error
    TypeError: Object.setPrototypeOf: expected an object or null, got undefined

--- src/db.ts
+++ src/db.ts
@@ -1,21 +1,3 @@
-import {Dexie} from "dexie";
-import {PuzzleOptions} from "./puzzle/Puzzle";
-
-export interface TimesItem extends PuzzleOptions {
-    time: number;
-    date: Date;
-}
-
-class EinsteinDatabase extends Dexie {
-    times: Dexie.Table<TimesItem, any>;
-
-    constructor() {
-        super("EinsteinDatabase");
-
-        this.version(1).stores({
-            times: "++, [rows+cols+extraHintsPercent]"
-        });
-    }
-}
+import {EinsteinDatabase} from "./EinsteinDatabase";
 
 export let db = new EinsteinDatabase();
