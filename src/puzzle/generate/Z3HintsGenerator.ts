d3bfe690907b32abff1a78f9a790dd6a3fec1789
Simmo Saan <simmo.saan@gmail.com>
2018-06-29 12:15:28 +0300

    Refactor Z3 hints generator

--- src/puzzle/generate/Z3HintsGenerator.ts
+++ src/puzzle/generate/Z3HintsGenerator.ts
@@ -6,72 +6,67 @@ import {DirectionHint} from "../hint/DirectionHint";
 import {Hint} from "../hint/Hint";
 import {OpenHint} from "../hint/OpenHint";
 import {SameColumnHint} from "../hint/SameColumnHint";
+import {PuzzleOptions} from "../Puzzle";
 import {SolvableHintsGenerator} from "./HintsGenerator";
 
 export class Z3HintsGenerator extends SolvableHintsGenerator {
+
+    private z3: Z3;
+    private ctx;
+
+    /*async generate(options: PuzzleOptions, board: SingleBoard): Promise<Hint[]> {
+        this.z3 = await getZ3();
+        this.makeContext();
+        this.assertBoard(board);
+
+        let hints = super.generate(options, board);
+
+        this.deleteContext();
+        return hints;
+    }*/
+
+    private makeContext() {
+        let cfg = this.z3.mk_config();
+        this.ctx = this.z3.mk_context(cfg);
+        this.z3.del_config(cfg);
+    }
+
+    private ask(smtlib2: string): string {
+        return this.z3.eval_smtlib2_string(this.ctx, smtlib2).trim();
+    }
+
+    private deleteContext() {
+        this.z3.del_context(this.ctx);
+    }
+
     async isSolvable(board: SingleBoard, hints: Hint[]): Promise<boolean> {
-        const z3 = await getZ3();
+        this.z3 = await getZ3();
+        this.makeContext();
+        this.assertBoard(board);
 
-        let cfg = z3.mk_config();
-        let ctx = z3.mk_context(cfg);
-        z3.del_config(cfg);
+        this.ask("(push)");
 
-        // z3.setParam("model", "true");
-        // z3.setParam("auto_config", "false");
-        // z3.setParam("smtlib2_compliant", "false");
+        this.assertHints(hints);
+        let checkSat = this.ask("(check-sat)");
+        let unique = checkSat == "unsat";
 
-        let ss = "";
-        let ask = s => {
-            // console.log(s);
-            ss += s + "\n";
-            return z3.eval_smtlib2_string(ctx, s);
-        };
+        this.ask("(pop)");
+        this.deleteContext();
+        return unique;
+    }
 
-        let setup = v => {
-            for (let row = 0; row < board.rows; row++) {
-                let xs = "";
-                for (let variant = 0; variant < board.variants; variant++) {
-                    let x = `${v}${row}${variant}`;
-                    ask(`(declare-const ${x} Int)`);
-                    ask(`(assert (and (<= 0 ${x}) (< ${x} ${board.cols})))`);
-                    xs += " " + x;
-                }
-
-                ask(`(assert (distinct${xs}))`);
+    private assertBoard(board: SingleBoard) {
+        for (let row = 0; row < board.rows; row++) {
+            let xs = "";
+            for (let variant = 0; variant < board.variants; variant++) {
+                let x = `x${row}${variant}`;
+                this.ask(`(declare-const ${x} Int)`);
+                this.ask(`(assert (and (<= 0 ${x}) (< ${x} ${board.cols})))`);
+                xs += " " + x;
             }
 
-            for (const hint of hints) {
-                if (hint instanceof AdjacentHint) {
-                    let x1 = `${v}${hint.row1}${hint.variant1}`;
-                    let x2 = `${v}${hint.row2}${hint.variant2}`;
-                    ask(`(assert (or (= ${x1} (+ ${x2} 1)) (= ${x1} (- ${x2} 1))))`);
-                }
-                else if (hint instanceof BetweenHint) {
-                    let x1 = `${v}${hint.row1}${hint.variant1}`;
-                    let xMiddle = `${v}${hint.rowMiddle}${hint.variantMiddle}`;
-                    let x2 = `${v}${hint.row2}${hint.variant2}`;
-                    ask(`(assert (or (and (= ${xMiddle} (+ ${x1} 1)) (= ${xMiddle} (- ${x2} 1))) (and (= ${xMiddle} (+ ${x2} 1)) (= ${xMiddle} (- ${x1} 1)))))`);
-                }
-                else if (hint instanceof DirectionHint) {
-                    let xLeft = `${v}${hint.rowLeft}${hint.variantLeft}`;
-                    let xRight = `${v}${hint.rowRight}${hint.variantRight}`;
-                    ask(`(assert (< ${xLeft} ${xRight}))`);
-                }
-                else if (hint instanceof OpenHint) {
-                    let x = `${v}${hint.row}${hint.variant}`;
-                    ask(`(assert (= ${x} ${hint.col}))`);
-                }
-                else if (hint instanceof SameColumnHint) {
-                    let x1 = `${v}${hint.row1}${hint.variant1}`;
-                    let x2 = `${v}${hint.row2}${hint.variant2}`;
-                    ask(`(assert (= ${x1} ${x2}))`);
-                }
-            }
-        };
-
-        setup("x");
-        // let sat1 = ask("(check-sat)").trim();
-        // let solvable = sat1 == "sat";
+            this.ask(`(assert (distinct${xs}))`);
+        }
 
         let ds = "";
         for (let row = 0; row < board.rows; row++) {
@@ -81,16 +76,36 @@ export class Z3HintsGenerator extends SolvableHintsGenerator {
                 ds += ` (distinct ${x} ${col})`;
             }
         }
-        ask(`(assert (or${ds}))`);
-        console.log(ss);
-        let sat2 = ask("(check-sat)").trim();
+        this.ask(`(assert (or${ds}))`);
+    }
 
-        let unique = sat2 == "unsat";
-        // alert(`${sat1} ${solvable}\n${sat2} ${unique}`);
-
-        z3.del_context(ctx);
-
-        // return solvable && unique;
-        return unique;
+    private assertHints(hints: Hint[]) {
+        for (const hint of hints) {
+            if (hint instanceof AdjacentHint) {
+                let x1 = `x${hint.row1}${hint.variant1}`;
+                let x2 = `x${hint.row2}${hint.variant2}`;
+                this.ask(`(assert (or (= ${x1} (+ ${x2} 1)) (= ${x1} (- ${x2} 1))))`);
+            }
+            else if (hint instanceof BetweenHint) {
+                let x1 = `x${hint.row1}${hint.variant1}`;
+                let xMiddle = `x${hint.rowMiddle}${hint.variantMiddle}`;
+                let x2 = `x${hint.row2}${hint.variant2}`;
+                this.ask(`(assert (or (and (= ${xMiddle} (+ ${x1} 1)) (= ${xMiddle} (- ${x2} 1))) (and (= ${xMiddle} (+ ${x2} 1)) (= ${xMiddle} (- ${x1} 1)))))`);
+            }
+            else if (hint instanceof DirectionHint) {
+                let xLeft = `x${hint.rowLeft}${hint.variantLeft}`;
+                let xRight = `x${hint.rowRight}${hint.variantRight}`;
+                this.ask(`(assert (< ${xLeft} ${xRight}))`);
+            }
+            else if (hint instanceof OpenHint) {
+                let x = `x${hint.row}${hint.variant}`;
+                this.ask(`(assert (= ${x} ${hint.col}))`);
+            }
+            else if (hint instanceof SameColumnHint) {
+                let x1 = `x${hint.row1}${hint.variant1}`;
+                let x2 = `x${hint.row2}${hint.variant2}`;
+                this.ask(`(assert (= ${x1} ${x2}))`);
+            }
+        }
     }
 }
